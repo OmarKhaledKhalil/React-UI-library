139e688a6bdf51cbbecb60c373e702e10a102435
OmarKhaledKhalil <omar.khaled.ahmed.khalil@gmail.com>
2024-09-30 05:13:36 +0300

    Add Cypress configuration and tests

---
+++ cypress/support/e2e.js
@@ -0,0 +1,7 @@
+// cypress/support/e2e.js
+// This file can include global configuration or hooks for Cypress
+
+// Example: This will run before each test suite
+before(() => {
+  console.log("Running before all tests");
+});
